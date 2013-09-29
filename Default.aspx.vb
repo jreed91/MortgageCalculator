7f7bff0252d72b5616881dc4dac02dc6a3325a98
Jacob Reed <jreed91@gmail.com>
2013-09-29 18:18:06 -0500

    Intial Code

---
+++ Default.aspx.vb
@@ -0,0 +1,69 @@
+﻿Imports System.Data
+
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub btnCalcPmt_Click(sender As Object, e As EventArgs) Handles btnCalcPmt.Click
+        'Declaring the Variables for each field
+        Dim loanAmount As Double
+        Dim annualRate As Double
+        Dim interestRate As Double
+        Dim term As Integer
+        Dim loanTerm As Integer
+        Dim monthlyPayment As Double
+
+
+        'Declare variables for Loan Amortization
+        Dim interestPaid As Double
+        Dim nBalance As Double
+        Dim principal As Double
+
+        'Declare a table to hold the payment info
+        Dim table As DataTable = New DataTable("ParentTable")
+        Dim loanAmortTbl As DataTable = New DataTable("AmortizationTable")
+        Dim tRow As DataRow
+
+        interestPaid = 0.0
+
+        loanAmount = CDbl(tbLoanAmt.Text)
+        annualRate = CDbl(tbAnnualInterest.Text)
+        term = CDbl(tbLoanTerm.Text)
+
+        tbLoanAmt.Text = FormatCurrency(loanAmount)
+
+        interestRate = annualRate / (100 * 12)
+
+        loanTerm = term * 12
+
+        monthlyPayment = loanAmount * interestRate / (1 - Math.Pow((1 + interestRate), (-loanTerm)))
+
+        lblMonthlyPmt.Text = FormatCurrency(monthlyPayment)
+
+
+        loanAmortTbl.Columns.Add("Payment Number", System.Type.GetType("System.String"))
+        loanAmortTbl.Columns.Add("Principal Paid", System.Type.GetType("System.String"))
+        loanAmortTbl.Columns.Add("Interest Paid", System.Type.GetType("System.String"))
+        loanAmortTbl.Columns.Add("New Balance", System.Type.GetType("System.String"))
+
+        Dim counterStart As Integer
+
+        For counterStart = 1 To loanTerm
+
+            interestPaid = loanAmount * interestRate
+            principal = monthlyPayment - interestPaid
+            nBalance = loanAmount - principal
+            loanAmount = nBalance
+
+            tRow = loanAmortTbl.NewRow()
+            tRow("Payment Number") = String.Format(counterStart)
+            tRow("Principal Paid") = String.Format("{0:C}", principal)
+            tRow("Interest Paid") = String.Format("{0:C}", interestPaid)
+            tRow("New Balance") = String.Format("{0:C}", nBalance)
+            loanAmortTbl.Rows.Add(tRow)
+
+        Next counterStart
+
+        loanGridView.DataSource = loanAmortTbl
+        loanGridView.DataBind()
+    End Sub
+End Class
